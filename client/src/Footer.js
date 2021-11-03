b14493427a8871783abce5f08f8dd1cd8164cd84
Somto Jude <ugwujude42@gmail.com>
2021-11-03 08:50:01 +0100

    initialized git

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+    <a href="" style={{color: 'white'}}>SomtoExchange</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
